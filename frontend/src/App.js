ea9f57893ce66d8307c5c464ef00fa5066a8d1e7
BuildTools <unconfigured@null.spigotmc.org>
2021-05-05 15:17:13 +0100

    Fix header position

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import HomePage from "./pages/HomePage";
-import ModulePage from "./pages/ModulePage";
-import CreateModulePage from "./pages/CreateModulePage";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import { Container } from "react-bootstrap";
-import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import ModulePage from './pages/ModulePage';
+import CreateModulePage from './pages/CreateModulePage';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,42 +1,45 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import logo from "./logo_white.png";
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from './logo_white.png';
+import { Container } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <meta charSet="utf-8" />
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Link to={`/`}>
-          <Navbar.Brand>
-            <img
-              src={logo}
-              alt="Brunel University London Logo "
-              style={{ width: "100px" }}
-            />
-          </Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="https://blackboard.brunel.ac.uk/" target="_blank">
-              Blackboard
-            </Nav.Link>
-            <Nav.Link
-              href="https://brunel.summon.serialssolutions.com/"
-              target="_blank"
-            >
-              Library
-            </Nav.Link>
-            <Nav.Link
-              href="https://europe.wiseflow.net/login/uk/brunel"
-              target="_blank"
-            >
-              WISEflow
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Container>
+          <Link to={`/`}>
+            <Navbar.Brand>
+              <img
+                src={logo}
+                alt="Brunel University London Logo "
+                style={{ width: '100px' }}
+              />
+            </Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="https://blackboard.brunel.ac.uk/" target="_blank">
+                Blackboard
+              </Nav.Link>
+              <Nav.Link
+                href="https://brunel.summon.serialssolutions.com/"
+                target="_blank"
+              >
+                Library
+              </Nav.Link>
+              <Nav.Link
+                href="https://europe.wiseflow.net/login/uk/brunel"
+                target="_blank"
+              >
+                WISEflow
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </header>
   );
